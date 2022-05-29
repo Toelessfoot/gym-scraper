eaa832eb0d73c07109c263ba1085b408bf191375
toelessfoot <toelessfoot@gmail.com>
2022-05-29 18:58:51 -0400

    test

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from time import sleep
 
-driver = webdriver.Chrom('./chromedriver.exe')
+options = Options()
+options.add_experimental_option("excludeSwitches", ['enable-logging'])
+options.add_argument(r'--user-data-dir=./chromeUserOne')
+
+driver = webdriver.Chrome("../chromedriver.exe", options=options)
 driver.get('https://emeraldcity2residents.buildinglink.com/v2/tenant/amenities/availabilitygrid.aspx')
 
-time = driver.find_elements_by_class_name('ReservationGridRowHeader-Inner')
-avail = driver.find_elements_by_class_name('ReservationGridCell-Inner')
-
-for i in range(len(time)):
-    print(str(i) + ' ' + time[i].text + ' ' + avail[i].text)
+#time = driver.find_elements_by_class_name('ReservationGridRowHeader-Inner')
+#avail = driver.find_elements_by_class_name('ReservationGridCell-Inner')
+#
+#for i in range(len(time)):
+#    print(str(i) + ' ' + time[i].text + ' ' + avail[i].text)
